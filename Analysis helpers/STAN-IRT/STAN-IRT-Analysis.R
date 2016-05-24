f237a120b1666ea39e7608b24246fe5e411e9793
drackham <drackham@gmail.com>
2016-05-23 23:45:55 -0600

    Package clean-up

---
+++ Analysis helpers/STAN-IRT/STAN-IRT-Analysis.R
@@ -0,0 +1,38 @@
+library('devtools')
+install_github("drackham/CDASimStudies", ref="develop")
+library("CDASimStudies")
+library('ggmcmc')
+library('rstan')
+library("shinystan")
+source("/Users/Dave/dev/EffectiveThetaBIN/Helpers/rootMeanSquaredDifference.R")
+
+# Load the files
+
+# Load the data
+data("IRT_1PL.1000")
+
+# Load the simulated parameter values
+thetaSim <- IRT_1PL.1000$theta
+betaSim <- IRT_1PL.1000$beta
+
+my_sso <- launch_shinystan(fit)
+
+post <- rstan::extract(fit, permuted = TRUE) # return a list of arrays
+
+thetaHat <- colMeans(post$theta)
+
+plot(thetaSim, thetaHat, xlim=c(-5,5), ylim=c(-5,5))
+abline(a=0,b=1)
+
+rmsd <- rootMeanSquaredDifference(thetaSim,thetaHat)
+mean(rmsd)
+plot(density(rmsd)) 
+
+betaHat <- colMeans(post$beta)
+
+plot(betaSim, betaHat, xlim=c(-5,5), ylim=c(-5,5))
+abline(a=0,b=1)
+
+rmsd <- rootMeanSquaredDifference(betaSim,betaHat)
+mean(rmsd)
+plot(density(rmsd)) 
