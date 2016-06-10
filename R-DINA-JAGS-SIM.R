dcd05c4c0893290f96e94310a674d8b6ed7b1992
drackham <drackham@gmail.com>
2016-06-09 22:26:34 -0600

    Refactored the JAGS simulation to accomodate the new runner

--- R-DINA-JAGS-SIM.R
+++ R-DINA-JAGS-SIM.R
@@ -10,56 +10,36 @@ library('parallel')
 library('runjags')
 
 # -------------------------------------------------------Command Line Setup---------------------------------------------------
-options(echo=TRUE) # if you want see commands in output file
-# args <- commandArgs(trailingOnly = TRUE)
+options(echo=FALSE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
 
-# simulationUUID <- UUIDgenerate()
-N <- 500
-data <- R_DINA_SimpleQ.500
-model <- "R-DINA-Non-Hierarchical.jags"
-maxCores <- min(2, parallel::detectCores()-1)
-iter <- 20000
-chains <- maxCores
+simulation_id <- as.numeric(args[1])
+dataset <- as.character(args[2])
+model <- as.character(args[3])
+N <- as.numeric(args[4])
+max_cores <- as.numeric(args[5])
+iter <- as.numeric(args[6])
+chains <- as.numeric(args[7])
+results_path <- as.character(args[8])
 
-# simulationUUID <- as.numeric(args[1])
-# N <- as.numeric(args[2])
-# data <- as.numeric(args[3])
-# model <- as.numeric(args[4])
-# maxCores <- as.numeric(args[5])
-# iter <- as.numeric(args[6])
-# chains <- cores
+cat("Simulation id is: ", simulation_id, "\n")
+cat("data is: ", dataset, "\n")
+cat("model is: ", model, "\n")
+cat("Sample Size is: ", N, "\n")
+cat("max_cores is: ", max_cores, "\n")
+cat("iterations is: ", iter, "\n")
+cat("chains is: ", chains, "\n")
+cat("results_path: ", results_path, "\n")
 
+# Set working directory
+setwd(results_path)
 
-# simPath <- paste("Results/Sim", simulationCondition, "/", sep="")
-# 
-# cat("Simulation Condition is: ", simulationCondition, "\n")
-# cat("Sample Size is: ", sampleSize, "\n")
-# cat("savedSteps is: ", savedSteps, "\n")
-# cat("thin is: ", thin, "\n")
-# cat("betaA is: ", betaA, "\n")
-# cat("betaB is: ", betaB, "\n")
-# cat("Model name is: ", jagsModel, "\n")
+# Load and save the simulated data 
+# http://stackoverflow.com/questions/9083907/r-how-to-call-an-object-with-the-character-variable-of-the-same-name
+data <- get(dataset)
 
-# Set the simID
-simID <- UUIDgenerate()
-
-# Set working directories
-wdLocal <- "~/Desktop/R-DINA-JAGS"
-wdRemote <- "/home/drackham/R-DINA-JAGS"
-wdSirThomas <- "/home/dave/dev/results"
-
-setwd(wdSirThomas)
-
-# Create simulation folder
-dir.create(simID)
-
-# Load and save the simulated data
-# N <- 500
-# data(R_DINA_SimpleQ_Flat.500)
-data(data)
 
 # Specify which model to use
-
 generateRDINAJagsNonHierachical()
 # generateRDINAJags()
 # generateHO_R_DINA_Jags()
@@ -67,15 +47,15 @@ generateRDINAJagsNonHierachical()
 # Start the timer!
 ptm <- proc.time()
 
-sim <- rDINAJagsSim(data, jagsModel = model, 
-                    maxCores = maxCores, adaptSteps = 1000, burnInSteps = 1000,
-										numSavedSteps = iter, thinSteps = 4)
+sim <- rDINAJagsSim(data, jagsModel = model,
+                    maxCores = max_cores, adaptSteps = 10, burnInSteps = 10,
+										numSavedSteps = iter, thinSteps = 1)
 
 # Stop the timer...
 duration <- proc.time() - ptm
 totalTime <- as.numeric(duration[3])
 
-save(sim, file = paste(simID, "/", simID, "-Sim.RData", sep=""))
+save(sim, file = "Sim.RData")
 
 #.......... Document the simulation ..............
 simType = model
@@ -85,7 +65,10 @@ dateStarted <- Sys.time()
 # Get the SHA1 that was used
 dcms.SHA1 <- unlist(strsplit(system("git ls-remote https://github.com/drackham/dcms develop", intern = TRUE), "\t"))[[1]] # Execute system command, split on \t unlist and keep only the SHA1
 
-simInfo <- data.frame(simID, simType, dateStarted, dcms.SHA1, dataSet, maxCores, iter, chains, totalTime)
+simInfo <- data.frame(simulation_id, simType, dateStarted, dcms.SHA1, dataSet, max_cores, iter, chains, totalTime)
 
 # Save the simInfo object
-write.table(simInfo, file = paste(simID, "/", simID, "-R-DINA Non-Hierarchical JAGS.txt", sep=""))
+save(simInfo, file = "simInfo.RData" )
+
+# Quit the R session and clean up
+quit(save = "no", status = 0, runLast = TRUE)
