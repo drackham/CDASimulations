40f6902fb4a7a4e9633fee8ce7f391babf46260a
drackham <drackham@gmail.com>
2016-06-11 14:30:25 -0600

    The seed now creates all sims against sir-thomas

--- sim_runner/seed.py
+++ sim_runner/seed.py
@@ -15,30 +15,30 @@ Sims_tracker.create(id=-1, type='sir-thomas test', dataset='R_DINA_SimpleQ.500',
 
 ########################### Low noise sims ###########################
 Sims_tracker.create(id=1, type='Low noise 50', dataset='R_DINA_SimpleQ_LN.50', model='R-DINA-Non-Hierarchical.jags',
-                          N=50, max_cores=4, iter=20000, chains=4, server=None)
+                          N=50, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=2, type='Low noise 200', dataset='R_DINA_SimpleQ_LN.200', model='R-DINA-Non-Hierarchical.jags',
-                          N=200, max_cores=4, iter=20000, chains=4, server=None)
+                          N=200, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=3, type='Low noise 1000', dataset='R_DINA_SimpleQ_LN.1000', model='R-DINA-Non-Hierarchical.jags',
-                          N=1000, max_cores=4, iter=20000, chains=4, server=None)
+                          N=1000, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 ########################### High noise sims ###########################
 Sims_tracker.create(id=4, type='High noise 50', dataset='R_DINA_SimpleQ_HN.50', model='R-DINA-Non-Hierarchical.jags',
-                          N=50, max_cores=4, iter=20000, chains=4, server=None)
+                          N=50, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=5, type='High noise 200', dataset='R_DINA_SimpleQ_HN.200', model='R-DINA-Non-Hierarchical.jags',
-                          N=200, max_cores=4, iter=20000, chains=4, server=None)
+                          N=200, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=6, type='High noise 1000', dataset='R_DINA_SimpleQ_HN.1000', model='R-DINA-Non-Hierarchical.jags',
-                          N=1000, max_cores=4, iter=20000, chains=4, server=None)
+                          N=1000, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 ########################### Uninformative sims ###########################
 Sims_tracker.create(id=7, type='Uninformative 50', dataset='R_DINA_SimpleQ_UnInform.50', model='R-DINA-Non-Hierarchical.jags',
-                          N=50, max_cores=4, iter=20000, chains=4, server=None)
+                          N=50, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=8, type='Uninformative 200', dataset='R_DINA_SimpleQ_UnInform.200', model='R-DINA-Non-Hierarchical.jags',
-                          N=200, max_cores=4, iter=20000, chains=4, server=None)
+                          N=200, max_cores=4, iter=20000, chains=4, server='sir-thomas')
 
 Sims_tracker.create(id=9, type='Uninformative 1000', dataset='R_DINA_SimpleQ_UnInform.1000', model='R-DINA-Non-Hierarchical.jags',
-                          N=1000, max_cores=4, iter=20000, chains=4, server=None)
+                          N=1000, max_cores=4, iter=20000, chains=4, server='sir-thomas')
